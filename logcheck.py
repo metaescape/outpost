b8deb570bc3595997bff4f7637bd639715085a5f
mE <metaescape@foxmail.com>
2023-08-21 18:10:52 +0800

    重构了解析 ip 部分，更加清晰，同时对 git 加入了 try 安全语句

--- logcheck.py
+++ logcheck.py
@@ -16,12 +16,18 @@ import fnmatch
 import requests
 from bs4 import BeautifulSoup
 import difflib
+import sys
+from pprint import pprint
+
 
 cnf = Config()
 SITE = cnf.httpd["sitename"]
-self_ips = cnf.ignore_ips["self_ips"]
-BOTS_LOOKUP = {"42.236.10.*": "360 spider"}
-server_ip = cnf.ignore_ips["server_ip"]
+BOTS_LOOKUP = cnf.bots_lookup
+SELF_IPS = cnf.ignore_ips["self_ips"]
+SERVER_IPS = cnf.ignore_ips["server_ip"]
+BOTS_LINK_KEYWORDS = cnf.bots_link_keywords
+BOTS_AGENT_KEYWORDS = cnf.bots_agent_keywords
+SELF_AGENT_MSGS = cnf.self_agent_msgs
 
 
 def parse_httpd_log(logline):
@@ -91,26 +97,23 @@ def extract_spider_brand(summary):
     return "unk bot"
 
 
-def get_resource(to):
+def access_static(to):
     """
     判断是否真的读取了文章，如果真的读取，一般会加载资源，例如 js, css,
     但由于浏览器缓存，重复访问是不会加载 js,css 的，因此需要加更多判断
     """
     return (
         "themes" in to
-        or "posts" in to
         or ".js" in to
         or ".png" in to
         or ".jpg" in to
         or ".gif" in to
+        or ".svg" in to
     )
 
 
 def get_success(info):
-    """
-    304 为使用缓存，因此说明不是第一次访问
-    """
-    return (info["return"] in ["200", "304"]) and (info["method"] == "GET")
+    return (info["return"] in ["200"]) and (info["method"] == "GET")
 
 
 def match_ip(ip, patterns):
@@ -152,7 +155,7 @@ def get_recent_logfiles(logfile, last):
     return files
 
 
-def logcheck(logfiles, old_hist, last):
+def full_fetch(logfiles, old_hist, last):
     if type(logfiles) == str:
         logfiles = get_recent_logfiles(logfiles, last)
 
@@ -183,7 +186,7 @@ def logcheck(logfiles, old_hist, last):
     with open(old_hist, "w", encoding="utf-8") as f:
         write_to_f_and_list(f"<h2>{SITE} 简报</h2> \n", f, mail_content)
         write_to_f_and_list(f"<p>{last} -> {now}</p>\n", f, mail_content)
-
+        gitnews = gitstar(last)
         if gitnews:
             for line in gitnews:
                 write_to_f_and_list(line, f, mail_content)
@@ -223,14 +226,98 @@ def logcheck(logfiles, old_hist, last):
     send_mail(cnf, "".join(mail_content))
 
 
+def bot_access(info, result_dict):
+    """
+    从 ip， from 和 agent summary 三个方面过滤爬虫
+    """
+    agent_summary = info["client"].lower()
+    if "bot" in agent_summary or "spider" in agent_summary:
+        result_dict["robots"].add(
+            (info["ip"], extract_spider_brand(agent_summary))
+        )
+        return True
+    bot = match_bot_ip(info["ip"], BOTS_LOOKUP)
+    if bot:
+        result_dict["robots"].add((info["ip"], bot))
+        return True
+    for keyword in BOTS_LINK_KEYWORDS:
+        if keyword in info["from"]:
+            result_dict["robots"].add((info["ip"], keyword))
+            return True
+    for keyword in BOTS_AGENT_KEYWORDS:
+        if keyword in agent_summary:
+            result_dict["robots"].add((info["ip"], keyword))
+            return True
+    return False
+
+
+def self_access(info):
+    """
+    根据 ip 网段和 agents 信息来过滤是否是自己访问
+    不过通过 ip 访问的也很有可能是云服务方的爬虫
+    """
+    if (
+        match_ip(info["ip"], SELF_IPS)
+        or SERVER_IPS in info["from"]
+        or SERVER_IPS in info["to"]
+    ):
+        return True
+    for msg in SELF_AGENT_MSGS:
+        if msg in info["client"]:
+            return True
+    return False
+
+
+def is_new_access_ip(info, result_dict):
+    """
+    判断当前访问是否访问了资源页，这是确定用户是否是初次访问的证据
+    """
+    if access_static(info["to"]) and (
+        info["from"].endswith("html") or info["from"].endswith("/")
+    ):
+        result_dict["full_visitors"].add(info["ip"])
+        return True
+
+
+def filter_true_visitors(result_dict, get_loc):
+    result_dict["attackers"] = [
+        x[0] for x in Counter(result_dict["fails"]).items() if x[1] >= 50
+    ]
+    # 继续过滤掉漏网的攻击者
+    non_attackers = [
+        x
+        for x in set(result_dict["normal_access"])
+        if x[0] not in result_dict["attackers"]
+    ]
+    robots = dict(result_dict["robots"])
+    # 继续过滤掉漏网的爬虫
+    non_robots = [x for x in non_attackers if x[0] not in robots]
+    for ip, access_page, from_link in non_robots:
+        if get_loc:
+            country, city = get_pos_from_ip(ip)
+        else:
+            country, city = "银河系", "漫游中"
+
+        freq = "再次"
+        if ip in result_dict["full_visitors"]:
+            freq = "初次"
+
+        if "html" in access_page:
+            from_loc = f"从 {from_link} " if from_link else " "
+            result_dict["content"].append(
+                f"<p> 来自 {country} {city} 的 {ip} {from_loc}{freq}访问了 {access_page} </p>"
+            )
+
+
 def collect_httpd_log(logfiles, last, get_loc=False):
     result_dict = {
         "logfile_status": [],
         "robots": set(),
-        "normal_acc": [],
+        "normal_access": [],
         "content": [],
         "fails": [],
         "attackers": [],
+        "full_visitors": set(),
     }
 
     for logfile in logfiles:
@@ -243,69 +330,28 @@ def collect_httpd_log(logfiles, last, get_loc=False):
                     continue
                 if info["datetime"] < last:
                     continue
-                success = get_success(info)
-                ip_summary = info["client"].lower()
-                bot = "bot" in ip_summary or "spider" in ip_summary
-                hit_page = info["to"][-5:] == ".html" or info["to"] == "/"
-                out_refer = (
-                    5 < len(info["from"]) < 100
-                    and "todayad.live" not in info["from"]
-                    and SITE not in info["from"]
-                )
-                full_get = get_resource(info["to"]) and "posts" in info["from"]
-                if bot:
-                    result_dict["robots"].append(
-                        (info["ip"], extract_spider_brand(ip_summary))
-                    )
-                elif match_bot_ip(info["ip"], BOTS_LOOKUP):
-                    result_dict["robots"].add(
-                        (info["ip"], match_bot_ip(info["ip"], BOTS_LOOKUP))
-                    )
-
-                elif not success:
+                if not get_success(info):
+                    # a lot of fails from one ip imply potential attackers
                     result_dict["fails"].append(info["ip"])
-                elif out_refer and hit_page:
-                    result_dict["normal_acc"].append(
-                        (info["ip"], info["to"], info["from"], info["return"])
-                    )
-                if (success and not bot) and full_get:
-                    data = (info["ip"], info["from"], "", info["return"])
-                    if (
-                        result_dict["normal_acc"]
-                        and data[:2] != result_dict["normal_acc"][-1][:2]
-                    ):
-                        result_dict["normal_acc"].append(data)
-
-            attackers = [
-                x[0]
-                for x in Counter(result_dict["fails"]).items()
-                if x[1] >= 200
-            ]
-            result_dict["attackers"] = attackers
-            no_att = [
-                x
-                for x in set(result_dict["normal_acc"])
-                if x[0] not in attackers
-            ]
-            robots_d = dict(result_dict["robots"])
-            no_bot = [x for x in no_att if x[0] not in robots_d]
-            for ip, page, refer, code in no_bot:
-                if (
-                    match_ip(ip, self_ips)
-                    or server_ip in refer
-                    or server_ip in page
-                ):
                     continue
-                if get_loc:
-                    country, city = get_pos_from_ip(ip)
-                else:
-                    country, city = "地球", "地球"
-                flag = "初次" if code == "200" else "再次"
-                if "html" in page:
-                    from_loc = f"从 {refer} " if refer else " "
-                    result_dict["content"].append(
-                        f"<p> 来自 {country} {city} 的 {ip}{from_loc}{flag}访问了 {page} </p>"
+                if bot_access(info, result_dict):
+                    continue
+                if self_access(info):
+                    continue
+                if is_new_access_ip(info, result_dict):
+                    # collect ips
+                    continue
+
+                if info["to"].endswith("html"):
+                    result_dict["normal_access"].append(
+                        (
+                            info["ip"],
+                            info["to"],
+                            info["from"],
+                        )
                     )
+
+    filter_true_visitors(result_dict, get_loc)
     return result_dict
 
 
@@ -355,6 +401,9 @@ def gitstar(last):
         repo_response = requests.get(
             url, headers={"Accept": "application/vnd.github.v3.star+json"}
         )
+        if repo_response.status_code != 200:
+            continue
+
         new_stars = []
         for i, star in enumerate(repo_response.json()):
             star_time = star["starred_at"]
@@ -365,7 +414,7 @@ def gitstar(last):
             if star_datetime > last:
                 new_stars.append(user)
         if new_stars:
-            gitcontent = (
+            gitcontent.append(
                 f"{repo} star 新增 {','.join(new_stars)}, 共 {i + 1} star\n"
             )
     return gitcontent
@@ -381,22 +430,47 @@ def time_in_range(start, end, x=None):
         return start <= x or x <= end
 
 
-def eager_fetch(logfile: str, watch_url, last):
-    mail_content = []
-    httpd_info = collect_httpd_log([logfile], last, True)
-    content = httpd_info["content"]
-    mail_content.extend(content)
+def eager_fetch(logfiles, watch_url, last, test=False):
+    """
+    尽管叫做 eager_fetch，但是实际上是一个完整的 logcheck 流程，只是用于执行更频繁场景
+    """
+    try:
+        mail_content = []
+        if type(logfiles) == str:
+            logfiles = [logfiles]
 
-    diff = check_and_save_html_changes(watch_url)
-    if diff:
-        mail_content.append("<h2>网页变化</h2>\n")
-        mail_content.extend(diff)
-    if mail_content:
-        fmt = "%Y年%m月%d日%H时%M分"
-        now = datetime.datetime.today().strftime(fmt)
-        cnf.mail["subject"] = f"{now} eager fetch report"
-        send_mail(cnf, "".join(mail_content))
-    print(mail_content)
+        httpd_info = collect_httpd_log(logfiles, last, get_loc=not test)
+        content = httpd_info["content"]
+        mail_content.extend(content)
+
+        diff = check_and_save_html_changes(watch_url)
+        if diff:
+            mail_content.append("<h2>网页变化</h2>\n")
+            mail_content.extend(diff)
+        new_star_msg = gitstar(last)
+        if new_star_msg:
+            mail_content.extend(new_star_msg)
+        if mail_content:
+            if not test:
+                fmt = "%Y年%m月%d日%H时%M分"
+                now = datetime.datetime.today().strftime(fmt)
+                cnf.mail["subject"] = f"{now} eager fetch report"
+                send_mail(cnf, "".join(mail_content))
+            else:
+                pprint(mail_content)
+
+    except Exception as e:
+        # 如果异常，发邮件提醒
+        cnf.mail["subject"] = f"eager fetch error: "
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        line_number = exc_traceback.tb_lineno
+        if not test:
+            if time_in_range(datetime.time(17, 0, 0), datetime.time(21, 0, 0)):
+                send_mail(
+                    cnf, f"An error occurred on line {line_number}: {str(e)}"
+                )
+        else:
+            pprint(line_number, e)
 
 
 def server():
@@ -411,13 +485,11 @@ def server():
     while 1:
         if time_in_range(start8, end8):
             last = datetime.datetime.today() - timedelta(hours=gap)
-            logcheck(logfile, "loghist.txt", last)
+            full_fetch(logfile, "loghist.txt", last)
 
         elif time_in_range(start8, end_day):
-            print("check")
             last = datetime.datetime.today() - timedelta(hours=interval)
             eager_fetch("/var/log/httpd/access_log", cnf.watch_url, last)
-            print("send")
         time.sleep(60 * 60 * interval)
 
 
@@ -425,22 +497,29 @@ def read_all():
     gap = 2000
     last = datetime.datetime.today() - timedelta(hours=gap)
     logfiles = glob.glob(f"{cnf.httpd['logfile']}*")
-    logcheck(logfiles, "loghist.txt", last)
+    full_fetch(logfiles, "loghist.txt", last)
 
 
 def test():
     gap = 2000
     last = datetime.datetime.today() - timedelta(hours=gap)
     logfiles = glob.glob(f"{cnf.httpd['logfile']}")
-    logcheck(logfiles, "loghist.txt", last)
+    full_fetch(logfiles, "loghist.txt", last)
 
 
-def test2():
+def test_eager_fetch():
     gap = 2000
     last = datetime.datetime.today() - timedelta(hours=gap)
     logfiles = glob.glob(f"/tmp/access_log*")
-    # return collect_httpd_log(logfiles, last)
-    check_and_save_html_changes("https://zhuanlan.zhihu.com/p/651112449")
+    return eager_fetch(
+        logfiles, "https://zhuanlan.zhihu.com/p/651112449", last, test=True
+    )
+
+
+def test_gitstar():
+    gap = 2000
+    last = datetime.datetime.today() - timedelta(hours=gap)
+    return gitstar(last)
 
 
 if __name__ == "__main__":
@@ -449,7 +528,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="选择要执行的函数")
     parser.add_argument(
         "--exec",
-        choices=["test", "read_all", "server", "test2"],
         required=True,
         help="要执行的函数名称",
     )
@@ -461,5 +539,7 @@ if __name__ == "__main__":
         read_all()
     elif args.exec == "server":
         server()
-    elif args.exec == "test2":
-        print(test2())
+    elif args.exec == "test-eager":
+        test_eager_fetch()
+    elif args.exec == "test-git":
+        pprint(test_gitstar())
